c934380a1110b0ec7be4d62feba7134e68fd6139
Natalia Shulzhenko <savostyuk@gmail.com>
2020-10-03 17:48:39 +0300

    init start typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0
+    else return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0
+    else return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0
+    else
+    {let sum = array.reduce((a, b) => a+b, 0);
+        let result = sum / array.length;
+        return result;}
 }
